13656d1e4cba71d402310bb0a3134c2073ec5011
Wangmoaza <haeun0512@hanmail.net>
2016-04-10 03:06:09 +0900

    First Commit

---
+++ Node.java
@@ -0,0 +1,45 @@
+public class Node<T> {
+    private T item;
+    private Node<T> next;
+
+    public Node(T obj) {
+        this.item = obj;
+        this.next = null;
+    }
+    
+    public Node(T obj, Node<T> next) {
+    	this.item = obj;
+    	this.next = next;
+    }
+    
+    public final T getItem() {
+    	return item;
+    }
+    
+    public final void setItem(T item) {
+    	this.item = item;
+    }
+    
+    public final void setNext(Node<T> next) {
+    	this.next = next;
+    }
+    
+    public Node<T> getNext() {
+    	return this.next;
+    }
+    
+    public final void insertNext(T obj) {   	
+    	Node<T> inserted = new Node<>(obj);
+    	inserted.setNext(this.next);
+    	setNext(inserted);
+    	
+    	//throw new UnsupportedOperationException("not implemented yet");
+    }
+    
+    public final void removeNext() {
+    	Node<T> removed = this.next;
+    	setNext(removed.getNext());
+		
+    	//throw new UnsupportedOperationException("not implemented yet");
+    }
+}
